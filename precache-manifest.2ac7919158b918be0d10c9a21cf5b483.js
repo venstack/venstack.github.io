98f52fe0d55a0981fc7f8c3d3ab12ec36ba90e88
vmaddineni2018 <vmaddineni2018@my.fit.edu>
2020-01-05 15:24:51 -0500

    Updates

---
+++ precache-manifest.2ac7919158b918be0d10c9a21cf5b483.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea72d40d6ebfc912d8e1f0173f6d1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8aa2bbe8a25f022d2f3",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "e4021b6167f1b4f6adb7",
+    "url": "/static/css/main.f533e5a5.chunk.css"
+  },
+  {
+    "revision": "d8aa2bbe8a25f022d2f3",
+    "url": "/static/js/2.7c9e9c4a.chunk.js"
+  },
+  {
+    "revision": "f0249686944f07ff5ae94108448e233c",
+    "url": "/static/js/2.7c9e9c4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4021b6167f1b4f6adb7",
+    "url": "/static/js/main.8d720fc1.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4926bdfb589ec527960a3db7de29f7b8",
+    "url": "/static/media/ic_apollo.4926bdfb.png"
+  },
+  {
+    "revision": "8631c6f7b31a24feba219eaac2807a3d",
+    "url": "/static/media/ic_aws.8631c6f7.png"
+  },
+  {
+    "revision": "d8ebc18624613dc5999d00f767b956e7",
+    "url": "/static/media/ic_docker.d8ebc186.png"
+  },
+  {
+    "revision": "4de70823f7292f2ccde4e208b443450f",
+    "url": "/static/media/ic_dotnet.4de70823.png"
+  },
+  {
+    "revision": "efbca4b675c5ca9aebc86a8dffab9c7a",
+    "url": "/static/media/ic_eclipse.efbca4b6.jpg"
+  },
+  {
+    "revision": "b7ec2c5f97eabac56f6f30ac60c7a1af",
+    "url": "/static/media/ic_express.b7ec2c5f.png"
+  },
+  {
+    "revision": "0660daf5c5b4d8e5e2c7a5dd298d5ee3",
+    "url": "/static/media/ic_flask.0660daf5.png"
+  },
+  {
+    "revision": "d3bac9260644a59dae85dd1984e6033c",
+    "url": "/static/media/ic_gcp.d3bac926.png"
+  },
+  {
+    "revision": "489f450e0ece4dd4c7b33de7d8c185aa",
+    "url": "/static/media/ic_golang.489f450e.png"
+  },
+  {
+    "revision": "193bc0f15ca22c41e2069d3469e5e5eb",
+    "url": "/static/media/ic_graphql.193bc0f1.png"
+  },
+  {
+    "revision": "43c3324c5fd6d4ec9cfd67762ed84468",
+    "url": "/static/media/ic_intellij.43c3324c.png"
+  },
+  {
+    "revision": "61ea41b740ef6560d7369c3d5cfda25f",
+    "url": "/static/media/ic_linkedin.61ea41b7.png"
+  },
+  {
+    "revision": "6b769a051fd51577f6b4189d734be9ff",
+    "url": "/static/media/ic_mongodb.6b769a05.png"
+  },
+  {
+    "revision": "0d14b9f22dc122f90c429ca7dbff317d",
+    "url": "/static/media/ic_nao_head.0d14b9f2.png"
+  },
+  {
+    "revision": "bcdde51b9daa0b6c62874da95328f78c",
+    "url": "/static/media/ic_sass.bcdde51b.png"
+  },
+  {
+    "revision": "0511565f30d0aa23e97d19ebb264e3d1",
+    "url": "/static/media/ic_visual_studio.0511565f.png"
+  },
+  {
+    "revision": "1c9246bae18ab96c0c502d211bc5996d",
+    "url": "/static/media/ic_xcode.1c9246ba.jpg"
+  },
+  {
+    "revision": "60caab3b13bad49f83190e7e5b8a199a",
+    "url": "/static/media/img_android.60caab3b.jpg"
+  },
+  {
+    "revision": "c5b4b23e9058fc3d79f30a2150309568",
+    "url": "/static/media/img_android_studio.c5b4b23e.png"
+  },
+  {
+    "revision": "6be43dce6f8d8cb61424ac79548687be",
+    "url": "/static/media/img_angular_js.6be43dce.png"
+  },
+  {
+    "revision": "58393424b2ab7ba7027f214660b11677",
+    "url": "/static/media/img_cpp.58393424.png"
+  },
+  {
+    "revision": "9c37711416e0e08d5f5aae96cda4f818",
+    "url": "/static/media/img_css_3.9c377114.png"
+  },
+  {
+    "revision": "8e107e269446ed212ccedc61fca50e3f",
+    "url": "/static/media/img_ios_3.8e107e26.png"
+  },
+  {
+    "revision": "c43424186b9c089b9aa1d64c7f1989c1",
+    "url": "/static/media/img_java.c4342418.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/img_javascript1.0cded3a3.png"
+  },
+  {
+    "revision": "9c9553413dab14c95628c705586176e5",
+    "url": "/static/media/img_jquery.9c955341.gif"
+  },
+  {
+    "revision": "8a613b5fe4da7dba1f630e14f42878c1",
+    "url": "/static/media/img_laravel.8a613b5f.png"
+  },
+  {
+    "revision": "35091c33ed0ff4b73eff9cef938d09cd",
+    "url": "/static/media/img_my_sql.35091c33.svg"
+  },
+  {
+    "revision": "6a716140ea15889fe5a87ab0ecac52b5",
+    "url": "/static/media/img_nodejs_3.6a716140.png"
+  },
+  {
+    "revision": "bd7ab5748a52ebb286ccd891d0054782",
+    "url": "/static/media/img_php.bd7ab574.png"
+  },
+  {
+    "revision": "e814db695213dd1b209242d602625781",
+    "url": "/static/media/img_publishat_new.e814db69.png"
+  },
+  {
+    "revision": "43fc453f508c05b7731a1a11b1daedd8",
+    "url": "/static/media/img_publishat_old.43fc453f.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/static/media/img_python.067050f6.png"
+  },
+  {
+    "revision": "2b18911e6827c7b4018a863e75bcf374",
+    "url": "/static/media/img_spring.2b18911e.png"
+  },
+  {
+    "revision": "0fdbb2459a5beddb97f8da66bc93ed86",
+    "url": "/static/media/img_srs.0fdbb245.png"
+  },
+  {
+    "revision": "39581203c969a4252eee275632030ccb",
+    "url": "/static/media/img_swift.39581203.png"
+  },
+  {
+    "revision": "9a7e9b3aceece906daa35e4f30d47238",
+    "url": "/static/media/img_typescript.9a7e9b3a.png"
+  },
+  {
+    "revision": "f0690f9c3829dcb86c60dd587f2eb445",
+    "url": "/static/media/img_vue_js.f0690f9c.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a9a00e120c5c77e17be4bef7ab4aa19f",
+    "url": "/static/media/myphoto1.a9a00e12.jpg"
+  },
+  {
+    "revision": "b2d23306f2293d73672a7c7ffc348729",
+    "url": "/static/media/venstack_main_logo.b2d23306.png"
+  }
+]);
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const githubURL = 'https://github.com/venstack';
-export const linkedinURL = 'https://www.linkedin.com/in/venkata-maddineni-95235218/';
